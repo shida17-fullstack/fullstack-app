572613108dd54b5b5acedcf6b318814bec9eab3d
shida17-fullstack <abogadanbutera@gmail.com>
2024-04-01 11:12:45 -0300

    Agregar archivos de sitio construidos

---
+++ site/build/precache-manifest.b5a4fdd4e93f03397eba1c9dd873dbc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1188b4254a39e5b52adb40d4b2ebb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d43933db3331979f79bb",
+    "url": "/static/css/main.2126833e.chunk.css"
+  },
+  {
+    "revision": "626b81a297b4696db48c",
+    "url": "/static/js/2.4f566358.chunk.js"
+  },
+  {
+    "revision": "851820dc40bca3bcaf80cf6dd26069f9",
+    "url": "/static/js/2.4f566358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d43933db3331979f79bb",
+    "url": "/static/js/main.0d83d6fc.chunk.js"
+  },
+  {
+    "revision": "d99c2fecd0c16b05759f",
+    "url": "/static/js/runtime-main.296d07e5.js"
+  }
+]);
